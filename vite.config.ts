e0cb42e256e5fb18b3647ac07bbefc46b2ba6a2e
Sanket Saha <sanketsaha@gmail.com>
2023-02-01 09:16:38 +0530

    Prod Build fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,14 +12,6 @@ export default defineConfig(({ command, mode }) => {
     define: {
       __APP_ENV__: env.APP_ENV,
       plugins: [react(),tsconfigPaths()],
-      server: {
-        watch: {
-          usePolling: true,
-        },
-        host: true, // needed for the Docker Container port mapping to work
-        strictPort: true,
-        port: 5173, // you can replace this port with any port
-      }
-    },
+    }
   }
 });
