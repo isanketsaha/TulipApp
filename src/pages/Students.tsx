1d68249ea2ddc8389398ce3569ab132ecdb2a32d
Sanket Saha <sanketsaha@gmail.com>
2023-02-20 10:26:14 +0530

    adding responsive for mobile

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -6,34 +6,36 @@ import { useState } from "react"
 import { useDebounce } from "../shared/hook/useDebounce"
 import { useAppSelector } from "../store"
 import { Role } from "../shared/utils/Role"
+import { useMediaQuery } from "react-responsive"
 
 export const StudentPage = () => {
+    const isMobile = useMediaQuery({ query: '(max-width: 700px)' })
     let navigate = useNavigate();
-    const {user} = useAppSelector(state => state.userAuth);
+    const { user } = useAppSelector(state => state.userAuth);
     const [searchValue, setSearchValue] = useState<string>("");
     const debouncedSearchTerm = useDebounce(searchValue, 500);
-    const { data, isFetching } = useSearchStudentByNameQuery(debouncedSearchTerm,{ skip: debouncedSearchTerm == "" });
+    const { data, isFetching } = useSearchStudentByNameQuery(debouncedSearchTerm, { skip: debouncedSearchTerm == "" });
 
     return (<>
         <Row >
-            <Col span={1} >
-              { user?.authority && [Role.ADMIN, Role.PRINCIPAL, Role.STAFF].includes(user?.authority) && <Link to="/admisssion" state={{
+            {isMobile ? null : <Col span={1} >
+                {user?.authority && [Role.ADMIN, Role.PRINCIPAL, Role.STAFF].includes(user?.authority) && <Link to="/admisssion" state={{
                     type: 'student'
                 }}>
                     <Button type="primary" htmlType="submit">
                         Student Enrollment
                     </Button>
                 </Link>}
-            </Col>
-            <Col span={6} offset={16}>
+            </Col>}
+            <Col span={isMobile ? 20 : 6} offset={isMobile ? 2 : 16}>
                 <Select
                     allowClear
                     showSearch placeholder="Student Name" size="large"
-                    onSelect={(id)=>navigate(`../studentDetails/${id}`)}
+                    onSelect={(id) => navigate(`../studentDetails/${id}`)}
                     onSearch={(value) => setSearchValue(value)} loading={isFetching} showArrow={false}
                     filterOption={false} style={{ width: '100%' }} notFoundContent={null} options={data?.map((d) => ({
                         value: d.id,
-                        label: <Button type="link" onClick={()=>navigate(`../studentDetails/${d.id}`)}> {d.name} </Button>,
+                        label: <Button type="link" onClick={() => navigate(`../studentDetails/${d.id}`)}> {d.name} </Button>,
                     }))} />
             </Col>
         </Row>
