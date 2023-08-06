6dbe426619d823a88cd6f89446e50c6e176c9987
Sanket Saha <sanketsaha@gmail.com>
2023-08-06 12:46:50 +0530

    added sort logic

---
+++ src/shared/utils/Const.ts
@@ -0,0 +1,15 @@
+
+export const monthToNumber: Record<string, number> = {
+    APRIL: 4,
+    MAY: 5,
+    JUNE: 6,
+    JULY: 7,
+    AUGUST: 8,
+    SEPTEMBER: 9,
+    OCTOBER: 10,
+    NOVEMBER: 11,
+    DECEMBER: 12,
+    JANUARY: 13,
+    FEBRUARY: 14,
+    MARCH: 15,
+  };
